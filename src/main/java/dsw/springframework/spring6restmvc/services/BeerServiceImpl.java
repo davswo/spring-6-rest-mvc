35f47bc62df320c16f2cd6fb1f69b0479d5a1424
David Swoboda <david.swoboda@dialogdata.de>
2024-12-11 08:07:30 +0100

    Add Controller with AllArgsConstructure

--- src/main/java/dsw/springframework/spring6restmvc/services/BeerServiceImpl.java
+++ src/main/java/dsw/springframework/spring6restmvc/services/BeerServiceImpl.java
@@ -2,12 +2,13 @@ package dsw.springframework.spring6restmvc.services;
 
 import dsw.springframework.spring6restmvc.model.Beer;
 import dsw.springframework.spring6restmvc.model.BeerStyle;
+import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
-
+@Service
 public class BeerServiceImpl implements BeerService {
     @Override
     public Beer getBeerById(UUID id) {
