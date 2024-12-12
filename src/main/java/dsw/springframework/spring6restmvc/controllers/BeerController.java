92918e17a6416e0f880ce928524c7f32c9006d51
David Swoboda <david.swoboda@dialogdata.de>
2024-12-12 07:25:25 +0100

    Add location header

--- src/main/java/dsw/springframework/spring6restmvc/controllers/BeerController.java
+++ src/main/java/dsw/springframework/spring6restmvc/controllers/BeerController.java
@@ -5,6 +5,7 @@ import dsw.springframework.spring6restmvc.services.BeerService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -28,9 +29,11 @@ public class BeerController {
     private final BeerService beerService;
 
     @PostMapping
-    public ResponseEntity<Beer> saveNewBeer(@RequestBody Beer beer) {
+    public ResponseEntity saveNewBeer(@RequestBody Beer beer) {
         final Beer savedNewBeer = beerService.saveNewBeer(beer);
-        return new ResponseEntity<>(savedNewBeer, HttpStatus.CREATED);
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Location", "/api/v1/beer/" + savedNewBeer.getId().toString());
+        return new ResponseEntity<>(headers, HttpStatus.CREATED);
     }
     
     @GetMapping
