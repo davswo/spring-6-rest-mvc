2563a9e6b67309668ef445ff340f827e03a37234
David Swoboda <david.swoboda@dialogdata.de>
2024-12-16 07:23:05 +0100

    Handle response status in exception

--- src/main/java/dsw/springframework/spring6restmvc/exceptions/NotFoundException.java
+++ src/main/java/dsw/springframework/spring6restmvc/exceptions/NotFoundException.java
@@ -1,5 +1,9 @@
 package dsw.springframework.spring6restmvc.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "Resource Not Found")
 public class NotFoundException extends RuntimeException {
 
 
